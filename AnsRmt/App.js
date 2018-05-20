fbdf828517ba339e36f668643fbf0cbe012336fc
Anselmo Eller <39027191+anselmoeller@users.noreply.github.com>
2018-05-20 19:02:55 -0300

    Update App.js

--- AnsRmt/App.js
+++ AnsRmt/App.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -12,27 +6,14 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+          Bem vindo ao React Native! {"\n"} Aqui é onde tudo começa!
+        </Text>        
       </View>
     );
   }
